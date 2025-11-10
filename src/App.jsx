11ca88289c9876746ef4544275845bc1d8cefabf
sunil kollu <sunil98-kollu>
2025-11-10 19:09:32 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import Child from './child'
-import UnControll from './UnControll'
 
 export default function App() {
-  const data ={
-    name:'react data props',
-    age:30
-  }
+ 
   return (
     <>
       <h1>react form</h1>
-      <Child/>
-      <UnControll/>
+    
     </>
   )
 }
